f4cdef3317f17186899c6fb05a9ea4c9bcb006d3
Igor Augusto <igoraugsa@gmail.com>
2025-09-03 00:01:38 -0300

    Added the "Create new Card" in the middle of page, did some css and started the right bar of properties

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,4 +2,14 @@ const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
+module.exports = {
+  theme: {
+    extend: {
+       dropShadow: {
+        'custom': ['0 200px 200px rgba(255, 51, 234, 1)']
+      }
+    }
+  }
+}
+
 export default config;
