2c73f952e35a82a9fb13279152cc6303a9e890b7
Igor Augusto <igoraugsa@gmail.com>
2025-09-04 09:54:47 -0300

    Added Content and Labels htmlcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,19 +65,76 @@ export default function CardCreator() {
         <div className="right-bar border-l-2">
           <h2 className='gradient-text'>Card Properties</h2>
           <span className='description'>Customize your study card</span>
+
           <div className="card-color">
             <span>BACKGROUND</span>
-            <div className="front-color">
-              <span>Front Background</span>
-              <div className='flex gap-2'>
-                <button className='w-10 h-10 bg-gradient-to-br from-[#A855F7] to-[#3B82F6] rounded-[8px]' title='color' type='button'/>
-                <button className='w-10 h-10 bg-gradient-to-br from-[#10B981] to-[#14B8A6] rounded-[8px]' title='color' type='button'/>
-                <button className='w-10 h-10 bg-gradient-to-br from-[#EC4899] to-[#EF4444] rounded-[8px]' title='color' type='button'/>
-                <button className='w-10 h-10 bg-gradient-to-br from-[#F97316] to-[#EAB308] rounded-[8px]' title='color' type='button'/>
-                <button className='w-10 h-10 bg-gradient-to-br from-[#4B5563] to-[#1F2937] rounded-[8px]' title='color' type='button'/>
-                <button className='w-10 h-10 bg-gradient-to-br from-[#6366F1] to-[#A855F7] rounded-[8px]' title='color' type='button'/>
+
+            <div className="front-color mt-4">
+              <span className='text-stone-400'>Front Background</span>
+
+              <div className='flex gap-2 mt-3'>
+                <button className='w-8 h-8 bg-gradient-to-br from-[#A855F7] to-[#3B82F6] rounded-[8px]' title='color' type='button'/>
+                <button className='w-8 h-8 bg-gradient-to-br from-[#10B981] to-[#14B8A6] rounded-[8px]' title='color' type='button'/>
+                <button className='w-8 h-8 bg-gradient-to-br from-[#EC4899] to-[#EF4444] rounded-[8px]' title='color' type='button'/>
+                <button className='w-8 h-8 bg-gradient-to-br from-[#F97316] to-[#EAB308] rounded-[8px]' title='color' type='button'/>
+                <button className='w-8 h-8 bg-gradient-to-br from-[#4B5563] to-[#1F2937] rounded-[8px]' title='color' type='button'/>
+                <button className='w-8 h-8 bg-gradient-to-br from-[#6366F1] to-[#A855F7] rounded-[8px]' title='color' type='button'/>
               </div>
 
+            </div>
+
+            <div className="back-color mt-4">
+              <span className='text-stone-400'>Back Background</span>
+
+              <div className='flex gap-2 mt-3'>
+                <button className='w-8 h-8 bg-gradient-to-br from-[#A855F7] to-[#3B82F6] rounded-[8px]' title='color' type='button'/>
+                <button className='w-8 h-8 bg-gradient-to-br from-[#10B981] to-[#14B8A6] rounded-[8px]' title='color' type='button'/>
+                <button className='w-8 h-8 bg-gradient-to-br from-[#EC4899] to-[#EF4444] rounded-[8px]' title='color' type='button'/>
+                <button className='w-8 h-8 bg-gradient-to-br from-[#F97316] to-[#EAB308] rounded-[8px]' title='color' type='button'/>
+                <button className='w-8 h-8 bg-gradient-to-br from-[#4B5563] to-[#1F2937] rounded-[8px]' title='color' type='button'/>
+                <button className='w-8 h-8 bg-gradient-to-br from-[#6366F1] to-[#A855F7] rounded-[8px]' title='color' type='button'/>
+              </div>
+
+            </div>
+          </div>
+
+          <div className="risk bg-zinc-600 w-full rounded-full h-0.5 "/>
+
+          {/*This div below is from Content Panel  */}
+          <div className="addContent flex flex-col gap-4">
+            <span>CONTENT</span>
+            <div className="create-panel cursor-pointer">
+              <span className='card-icon from-zinc-500 to-stone-400 leading-8'>T</span>
+              <div className="titleDesc flex flex-col">
+                <span className=''>Add Text</span>
+                <span className='text-stone-400 text-[.9rem]'>Add Text to your cards</span>
+              </div>
+            </div>
+
+            <div className="create-panel cursor-pointer">
+              <span className='card-icon from-purple-500 to-pink-400 leading-6'>📷</span>
+              <div className="titleDesc flex flex-col">
+                <span className=''>Add Image</span>
+                <span className='text-stone-400 text-[.9rem]'>JPG, PNG, GIF up to 5MB</span>
+              </div>
+            </div>
+
+            <div className="create-panel cursor-pointer">
+              <span className='card-icon from-blue-500 to-cyan-500 leading-9'>🎵</span>
+              <div className="titleDesc flex flex-col">
+                <span className=''>Add Audio</span>
+                <span className='text-stone-400 text-[.9rem]'>MP3, WAV up to 10MB</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="labels flex flex-col gap-3">
+            <span className=''>LABELS & TAGS</span>
+            <input className='create-panel' title="Labels" placeholder="Add Tags (press Enter)" type="text"></input>
+            <div className="show-label flex justify-between scroll-auto">
+              <button className='variable bg-gradient-to-r outline-purple-500/70 from-purple-500/40 to-pink-500/40 rounded-full w-fit'>JavaScript</button>
+              <button className='variable bg-gradient-to-r outline-cyan-500/70 from-cyan-500/40 to-blue-500/40 rounded-full w-fit'>Variables</button>
+              <button className='variable bg-gradient-to-r outline-lime-500/70 from-lime-500/40 to-green-500/40 rounded-full w-fit'>Programming</button>
             </div>
           </div>
 
